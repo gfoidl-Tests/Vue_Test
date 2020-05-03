ae9ba3554800c8344e0476c75816ac5b40e0c6cb
Günther Foidl <gue@korporal.at>
2020-05-03 15:47:30 +0200

    jest tests broken into projects

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,9 @@
-const { pathsToModuleNameMapper } = require("ts-jest/utils");
-const { compilerOptions }         = require("./tsconfig");
-//-----------------------------------------------------------------------------
 module.exports = {
-    moduleFileExtensions: ["js", "ts", "json", "vue"],
-    transform           : {
-        "^.+\\.vue$": "vue-jest",
-        "^.+\\.ts?$": "ts-jest"
-    },
-    testMatch: [
-        "**/tests/unit/**/*.[tj]s",
-        "**/tests/vue/**/*.[tj]s"
-    ],
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-        prefix: "<rootDir>/"
-    }),
-    // helper (above) is used to automate this
-    //moduleNameMapper: {
-    //    "^@/(.*)$"   : "<rootDir>/src/ts/$1",
-    //    "^@svc/(.*)$": "<rootDir>/src/ts/services/$1"
-    //},
-    coverageReporters: ["text", "cobertura", "html"],   // https://github.com/istanbuljs/istanbuljs/tree/master/packages/istanbul-reports/lib
-    globals: {
-        VERSION: "42"
-    }
+    projects: [
+        // wildcards are supported like
+        // "<rootDir>/tests/*"
+        // but I want to exclude e2e here
+        "<rootDir>/tests/unit",
+        "<rootDir>/tests/vue"
+    ]
 };
