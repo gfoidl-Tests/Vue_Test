7c412cdd190f1bb23150a128ddd3aeb22e06bf17
Günther Foidl <gue@korporal.at>
2020-05-05 10:31:36 +0200

    Fixed publicPath and resolution + added some test-files to verify

--- src/ts/app.vue
+++ src/ts/app.vue
@@ -1,5 +1,6 @@
 <template>
-    <main-view></main-view>
+    <!--<main-view></main-view>-->
+    <loading></loading>
 </template>
 
 <style lang="less" scoped>
@@ -7,10 +8,13 @@
 
 <script lang="ts">
     import { Vue, Component } from "vue-property-decorator";
+
+    import Loading from "@cmp/loading.vue";
     //-------------------------------------------------------------------------
     @Component({
         components: {
-            "MainView": () => import(/* webpackChunkName: "main" */ "@views/main.vue")
+            "MainView": () => import(/* webpackChunkName: "main" */ "@views/main.vue"),
+            Loading
         }
     })
     export default class App extends Vue {
---
+++ src/ts/components/loading.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="loading"></div>
+        <b-img src="@img/foo.jpg" width="100px"></b-img>
+        <img src="@img/foo.jpg" width="100" />
+        <b-img src="@img/car.svg" width="100px"></b-img>
+        <b-img src="@img/decimal.svg"></b-img>
+    </div>
+</template>
+
+<style lang="less" scoped>
+    .loading {
+        background-image : url("loading.gif");
+        background-repeat: no-repeat;
+        margin           : 0 auto;
+        width            : 200px;
+        height           : 200px;
+    }
+</style>
