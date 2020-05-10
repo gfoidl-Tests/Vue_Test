b32a4db50f8af1d22c52e19f9cd8e5b21d406b3b
Günther Foidl <gue@korporal.at>
2020-05-10 14:04:50 +0200

    Eventhandlers for push and notificationclick

--- src/ts/service-worker.ts
+++ src/ts/service-worker.ts
@@ -14,3 +14,28 @@ cleanupOutdatedCaches();
 
 // Mandatory for workbox plugin to set the assets to cache
 precacheAndRoute(self.__WB_MANIFEST);
+//-----------------------------------------------------------------------------
+self.addEventListener("push", event => {
+    const notificationPromise = self.registration.showNotification("Vue Test", {
+        icon: "calculator.png",
+        body: event.data?.text()
+    });
+
+    event.waitUntil(notificationPromise);
+});
+
+self.addEventListener("notificationclick", event => {
+    console.log("Notification clicked");
+
+    const promise = self.clients.matchAll().then(clients => {
+        // Check if at least one client is already open
+        if (clients.length) {
+            const windowClient = clients[0] as WindowClient;
+            windowClient.focus();
+        } else {
+            self.clients.openWindow("/");
+        }
+    });
+
+    event.waitUntil(promise);
+});
