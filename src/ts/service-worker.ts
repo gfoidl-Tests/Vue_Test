844f8c13cb4b28c8cc1f7b4e333bc4b90c5ac53c
Günther Foidl <gue@korporal.at>
2020-05-10 14:56:41 +0200

    Actions for notification

--- src/ts/service-worker.ts
+++ src/ts/service-worker.ts
@@ -15,27 +15,79 @@ cleanupOutdatedCaches();
 // Mandatory for workbox plugin to set the assets to cache
 precacheAndRoute(self.__WB_MANIFEST);
 //-----------------------------------------------------------------------------
-self.addEventListener("push", event => {
-    const notificationPromise = self.registration.showNotification("Vue Test", {
-        icon: "calculator.png",
-        body: event.data?.text()
+async function handlePush(event: PushEvent): Promise<void> {
+    // This uncommented block will lead to the following exception:
+    //  Failed to execute 'showNotification' on 'ServiceWorkerRegistration': No notification permission has been granted for this origin.
+    // So use the code below, which does the job. Note: without the check and when
+    // there is no permission, an exception is thrown.
+    //const permission = await self.registration.pushManager.permissionState();
+    //if (permission !== "granted") {
+    //    return;
+    //}
+
+    if (Notification.permission !== "granted") {
+        console.log("Notification received, but permission to show is not granted.");
+        return;
+    }
+
+    await self.registration.showNotification("Vue Test", {
+        icon   : "calculator.png",
+        body   : event.data?.text(),
+        actions: [
+            {
+                action: "open",
+                title : "Open app"
+            },
+            {
+                action: "close",
+                title : "Close notification"
+            }
+        ]
     });
+}
+//-----------------------------------------------------------------------------
+async function handleNotificationClicked(event: NotificationEvent): Promise<void> {
+    if (!event.action) {
+        console.log("Normal notification click");
+        return;
+    }
 
-    event.waitUntil(notificationPromise);
-});
+    if (event.action === "close") {
+        console.log("Notification close clicked");
+        event.notification.close();
+        return;
+    }
 
-self.addEventListener("notificationclick", event => {
-    console.log("Notification clicked");
+    console.log("Notification open clicked");
 
-    const promise = self.clients.matchAll().then(clients => {
-        // Check if at least one client is already open
-        if (clients.length) {
-            const windowClient = clients[0] as WindowClient;
-            windowClient.focus();
-        } else {
-            self.clients.openWindow("/");
+    const urlToOpen = self.location.href;
+    const clients   = await self.clients.matchAll({
+        type               : "window",
+        includeUncontrolled: true
+    });
+    const windowClients = clients.map(c => c as Readonly<WindowClient>);
+    let matchingClient: WindowClient | null = null;
+
+    for (const windowClient of windowClients) {
+        if (windowClient.url === urlToOpen) {
+            matchingClient = windowClient;
+            break;
         }
-    });
+    }
 
-    event.waitUntil(promise);
+    if (matchingClient) {
+        await matchingClient.focus();
+    } else {
+        await self.clients.openWindow(urlToOpen);
+    }
+
+    event.notification.close();
+}
+//-----------------------------------------------------------------------------
+self.addEventListener("push", event => {
+    event.waitUntil(handlePush(event));
+});
+//-----------------------------------------------------------------------------
+self.addEventListener("notificationclick", event => {
+    event.waitUntil(handleNotificationClicked(event));
 });
