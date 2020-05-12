df1575941962c941b6f7e866b06486265aa110b7
Günther Foidl <gue@korporal.at>
2020-05-12 14:12:30 +0200

    Due to incognito the db-storage.ts tests can be simplified

--- tests/e2e/db-storage.ts
+++ tests/e2e/db-storage.ts
@@ -1,68 +1,20 @@
-import * as puppeteer from "puppeteer";
-import Helper         from "./helper";
+import Helper from "./helper";
 //-----------------------------------------------------------------------------
 describe("Storage in DB", () => {
     const baseUrl = "http://localhost:8080/";
-    let browser: puppeteer.Browser;
-    let myPage : puppeteer.Page;
-    //-------------------------------------------------------------------------
-    beforeAll(async () => {
-        browser = await puppeteer.launch({
-            headless         : true,
-            ignoreHTTPSErrors: true
-        });
-    });
     //-------------------------------------------------------------------------
     beforeEach(async () => {
-        myPage = await browser.newPage();
-
         // Uncomment for better debugging
-        //myPage.on("console", msg => {
+        //page.on("console", msg => {
         //    const text = msg.text();
         //    console.log("[PAGE LOG] ", text);
         //});
 
-        await myPage.goto(baseUrl);
+        await page.goto(baseUrl);
     });
     //-------------------------------------------------------------------------
     afterEach(async () => {
-        await myPage.evaluate(() => {
-            return new Promise((resolve, reject) => {
-                const openRequest = window.indexedDB.open("calc");
-
-                openRequest.onsuccess = () => {
-                    const db               = openRequest.result;
-                    const transaction      = db.transaction("input", "readwrite");
-                    transaction.oncomplete = () => resolve();
-                    transaction.onerror    = () => reject(transaction.error);
-
-                    const store        = transaction.objectStore("input");
-                    const clearRequest = store.clear();
-
-                    // Not necessary, it's "done" via the transaction. This event is triggered
-                    // before the tran completes (as several actions can be grouped by a transaction ;-))
-                    clearRequest.onsuccess = () => {
-                        console.log("store input cleared");
-                    }
-                }
-
-                openRequest.onerror = () => {
-                    console.error("error opening db", openRequest.error);
-                    reject(openRequest.error);
-                }
-            });
-        });
-
-        await myPage.close();
-    });
-    //-------------------------------------------------------------------------
-    afterAll(async () => {
-        await browser.close();
-    });
-    //-------------------------------------------------------------------------
-    test("fresh page -> calc-db is empty", async () => {
-        const count = await Helper.checkCountOfInputStore(myPage);
-        expect(count).toBe(0);
+        await jestPuppeteer.resetBrowser();
     });
     //-------------------------------------------------------------------------
     test("calculation -> inputs stored to db", async () => {
@@ -70,65 +22,70 @@ describe("Storage in DB", () => {
         // for reason why jest-puppeteer is useful.
 
         // jest-puppeteer
-        await expect(myPage).toFill("#input-a", "3");
+        await expect(page).toFill("#input-a", "3");
 
         // pure puppeteer (long version)
-        //const inputB = await expect(myPage).toMatchElement("#input-b");
+        //const inputB = await expect(page).toMatchElement("#input-b");
         //await inputB.focus();
-        //await myPage.keyboard.type("4");
+        //await page.keyboard.type("4");
 
         // pure puppeteer (short version)
-        await myPage.type("#input-b", "4");
+        await page.type("#input-b", "4");
 
         // pure puppeteer
-        //const inputOp = await expect(myPage).toMatchElement("#input-op");
+        //const inputOp = await expect(page).toMatchElement("#input-op");
         //await inputOp.select("Subtract");
         // jest-puppeteer
-        await expect(myPage).toSelect("#input-op", "Multiply");
+        await expect(page).toSelect("#input-op", "Multiply");
 
         // pure puppeteer
-        //await myPage.click("#calcButton");
+        //await page.click("#calcButton");
         // jest-puppeteer
-        await expect(myPage).toClick("#calcButton");
+        await expect(page).toClick("#calcButton");
 
-        await Helper.takeScreenshot("db-storage_form-filled.png", myPage);
+        await Helper.takeScreenshot("db-storage_form-filled.png", page);
 
-        const count = await Helper.checkCountOfInputStore(myPage);
+        const count = await Helper.checkCountOfInputStore(page);
         expect(count).toBe(1);
     });
     //-------------------------------------------------------------------------
     test("reset after calculation -> inputs cleared in db", async () => {
-        await expect(myPage).toFill("#input-a", "3");
-        await expect(myPage).toFill("#input-b", "4");
-        await expect(myPage).toSelect("#input-op", "Multiply");
-        await expect(myPage).toClick("#calcButton");
+        await expect(page).toFill("#input-a", "3");
+        await expect(page).toFill("#input-b", "4");
+        await expect(page).toSelect("#input-op", "Multiply");
+        await expect(page).toClick("#calcButton");
 
         // Wait a little bit, then click reset
-        await myPage.waitFor(250);
-        await expect(myPage).toClick("#resetButton");
+        await page.waitFor(250);
+        await expect(page).toClick("#resetButton");
 
-        await Helper.takeScreenshot("db-storage_form-reset.png", myPage);
+        await Helper.takeScreenshot("db-storage_form-reset.png", page);
 
-        const count = await Helper.checkCountOfInputStore(myPage);
+        const count = await Helper.checkCountOfInputStore(page);
         expect(count).toBe(0);
     });
     //-------------------------------------------------------------------------
     test("page load after previous calculation -> inputs from db are set", async () => {
-        await expect(myPage).toFill("#input-a", "3");
-        await expect(myPage).toFill("#input-b", "4");
-        await expect(myPage).toSelect("#input-op", "Divide");
-        await expect(myPage).toClick("#calcButton");
+        await expect(page).toFill("#input-a", "3");
+        await expect(page).toFill("#input-b", "4");
+        await expect(page).toSelect("#input-op", "Divide");
+        await expect(page).toClick("#calcButton");
 
         // Navigate away, then back
-        await myPage.goto("https://google.at");
-        await myPage.goBack();
+        await page.goto("https://google.at");
+        await page.goBack();
 
-        const inputA  = await expect(myPage).toMatchElement("#input-a");
-        const inputB  = await expect(myPage).toMatchElement("#input-b");
-        const inputOp = await expect(myPage).toMatchElement("#input-op");
+        const inputA  = await expect(page).toMatchElement("#input-a");
+        const inputB  = await expect(page).toMatchElement("#input-b");
+        const inputOp = await expect(page).toMatchElement("#input-op");
 
-        await expect(myPage.evaluate(el => el.value, inputA)) .resolves.toBe("3");
-        await expect(myPage.evaluate(el => el.value, inputB)) .resolves.toBe("4");
-        await expect(myPage.evaluate(el => el.value, inputOp)).resolves.toBe("Divide");
+        await expect(page.evaluate(el => el.value, inputA)) .resolves.toBe("3");
+        await expect(page.evaluate(el => el.value, inputB)) .resolves.toBe("4");
+        await expect(page.evaluate(el => el.value, inputOp)).resolves.toBe("Divide");
+    });
+    //-------------------------------------------------------------------------
+    test("fresh page -> calc-db is empty", async () => {
+        const count = await Helper.checkCountOfInputStore(page);
+        expect(count).toBe(0);
     });
 });
