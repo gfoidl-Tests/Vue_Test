54014ddf6973b7cc08910754061cd28051a2981a
Günther Foidl <gue@korporal.at>
2020-05-12 11:53:22 +0200

    Run E2E-Tests in incognito
    To avoid sharing the browser context between tests and have "fresh" install of workers, dbs, etc.

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -6,7 +6,10 @@ module.exports = {
         slowMo  : process.env.SLOWMO,
         dumpio  : process.env.DUMPIO
     },
-    server: {
+    // User Incognito to avoid sharing the browser context between tests
+    // https://github.com/smooth-code/jest-puppeteer/blob/master/packages/jest-environment-puppeteer/README.md#jest-puppeteerconfigjs
+    browserContext: "incognito",
+    server        : {
         command : "yarn serve dist --no-clipboard --listen 8080",
         port    : 8080,
         protocol: "http"
